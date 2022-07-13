0507865bbb4b24b0d48730b45f656b8c70a98f88
Anirudh <yanirudha978@gmail.com>
2022-07-13 21:32:52 +0530

    first code

---
+++ JavaClass/dataType.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+// O(n)
+// O(n)
+
+class dataType {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        // int n = sc.nextInt();
+
+        // char ch = 'a';
+
+
+        // if(ch == 'a'){
+        //     System.out.println(n);
+        // }
+
+        // String name = new String("Anirudh");
+
+        // String name1 = "Hello there";
+
+
+
+        // System.out.println(name);
+
+        int age = 20;
+
+        if(age>= 25){
+            System.out.println("You are experienced");
+        }
+
+
+
+    }
+}

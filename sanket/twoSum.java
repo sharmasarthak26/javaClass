ca57daff4cc12eaf8f59528b1127e783db44c3bf
sanket8700 <sankets101btechcse2023@kccitm.edu.in>
2022-07-25 09:54:36 +0530

    assignment

---
+++ sanket/twoSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int arr=nums.length;
+        for(int i=0; i<arr;i++){
+            
+            for(int j=i+1;j<arr;j++){
+                if(nums[j]==target -nums[i]){
+                    
+                    return new int[]{i,j};
+                }
+                
+            }
+           
+        }
+       
+        
+    
+    return null;
+    }
+}

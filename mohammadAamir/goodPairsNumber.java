37c17a54582c382d5ecd14351c26d9479213d23a
Md Aamir <asadahmadkh344@gmail.com>
2022-07-25 19:18:47 +0530

    Good Pairs

---
+++ mohammadAamir/goodPairsNumber.java
@@ -0,0 +1,25 @@
+package mohammadAamir;
+
+//Time Complexity:O(N2)
+//Space Complexity:O(1)
+
+public class goodPairsNumber {
+
+	public static void main(String[] args) {
+		int []nums = {1,2,3,1,1,2};
+		
+		  int count=0;
+		    for(int i=0; i<nums.length; i++){
+		        for(int j=0; j<nums.length; j++){
+		            if(i<j){
+		               if(nums[i]==nums[j]){
+		                    count++;     
+		               } 
+		            }
+		        }
+		    }
+		    System.out.println(count);
+
+	}
+
+}
